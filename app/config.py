84ab2a0eb976f35be0f6af77e20ef04a07810e1d
Evgen-Jekov <zekovevgen992@gmail.com>
2025-03-20 01:15:20 +0000

    connect to postgresql

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     CELERY_BROKER_URL = os.getenv('REDIS')
     CELERY_RESULT_BACKEND = os.getenv('REDIS')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQL_POSTGRE')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
