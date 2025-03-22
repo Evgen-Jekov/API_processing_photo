debb583636cdf6b05cd6fbc8f96c9ec02d377983
Evgen-Jekov <zekovevgen992@gmail.com>
2025-03-22 02:09:22 +0000

    User resource created

---
+++ app/resource/user/account.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.user.user import UserModel
+
+class UserAccount(Resource):
+    @jwt_required()
+    def get(self):
+        try:
+            user = UserModel.query.filter(UserModel.id == get_jwt_identity).first()
+
+            if not user:
+                return {"error" : "user found not"}, 404
+        except Exception as e:
+            return {"error database" : str(e)}, 500
+
+        return {"username" : user.username}
