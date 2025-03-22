7d62cf105b324d420d57dbf85e1e5e703fe36745
Evgen-Jekov <zekovevgen992@gmail.com>
2025-03-22 02:40:46 +0000

    the return data has been changed /account

--- app/resource/user/account.py
+++ app/resource/user/account.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from app.models.user.user import UserModel
+from app.marshmallow.user.user_scheme import UserSchema
 
 class UserAccount(Resource):
     @jwt_required()
@@ -13,4 +14,5 @@ class UserAccount(Resource):
         except Exception as e:
             return {"error database" : str(e)}, 500
 
-        return {"username" : user.username}
+        user_detail = UserSchema().dump(user)
+        return {"username" : user}, 200
